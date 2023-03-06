d90e1f2faa57b6ce1a86eec7c11ce2f1e5ebc172
lizramos611 <liz.ramos@aim.com>
2023-03-06 18:53:49 -0500

    adding in SQL questions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
-//requiring sql and inquirer 
+//requiring sql and inquirer and express
+
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const conTable = require('console.table');
@@ -8,6 +9,7 @@ const conTable = require('console.table');
 const newRole = require('./newrole');
 const newDepartment = require('./newdepartment');
 const newEmployee = require('./newemployee');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 //connecting to my sql db
 const db = mysql.createConnection (
@@ -31,17 +33,60 @@ name: 'first',
 message: 'What would you like to do first?',
 choices: [
     "View all Employees",
-    'Add Roles',
-    'Add Employees',
+    'Add New Role',
+    'Add New Department',
+    'Add New Employee',
     "View All Departments",
     'View All Roles',
     'View All Employees',
     'Update An Employees Role',
+    'All Done!'
 
 ]
 
 }
 
 ])
+.then(function(data) {
+    if(data.first == "View All Employees"){
+        db.query('SELECT * FROM employee', function (err, results){
+            return questions();
+        });
+    } else if (data.first == "View All Departments") {
+        return questions();
+    } else if(data.first == "Add New Department"){
+        addDepartment();
+    } else if (data.first == "Add New Employee") {
+        addEmployee();
+    } else if (data.first == "Add New Role") {
+        addRole();
+    }else if(data.first === "Update An Employees Role"){
+        return questions();
+    } else if(data.first === "All Done!"){
+        process.exit;
+    }
+})
 
 };
+
+const addDepartment = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'departmentName',
+        message: 'What is the name of the new department?',
+
+    }])
+    .then(function(department){
+
+        const newDepartmentAdded = new newDepartment (department.departmentName)
+        const name = newDepartmentAdded.name
+        
+        db.query('INSERT INTO department VALUES (?)', name, (err,results) => {
+            if (err) {
+                return questions();
+            } else {
+                return questions();
+            }
+        }
+    })
+}
