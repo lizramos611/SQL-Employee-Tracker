474645ef667efb9c59b14595cbd2266c47e48b56
lizramos611 <liz.ramos@aim.com>
2023-03-08 18:34:32 -0500

    adding in module export folders

---
+++ js/newdepartment.js
@@ -0,0 +1,6 @@
+function Department(name) {
+    this.name = name;
+
+}
+
+module.exports = Department;
---
+++ js/newrole.js
@@ -0,0 +1,7 @@
+function Role (title, pay, department_id) {
+    this.title = title;
+    this.pay = pay;
+    this.department_id = department_id;
+}
+
+module.exports = Role;
